84d2bd4e9c1297c0b93568a5c4b79c25c0fe214f
n-a-t-e-d-a-w-g <nathangvella@gmail.com>
2024-04-23 17:24:48 -0600

    adding socials

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,19 @@ export default function Home() {
         height={250} 
       />
       <div className="mt-4 sm:mt-0 sm:ml-4">
-        <h1 className="text-4xl font-bold mb-4">Nathan Vella</h1>
-        <p className="text-lg">Just trying to build things.</p>
+        <h1 className="text-4xl font-bold mb-4">Nate Vella</h1>
+        <p className="text-lg">Just trying to build stuff</p>
+        <div className="mt-4 flex justify-start"> {/* Modified line */}
+          <a href="https://www.linkedin.com/in/vellanathan/" target="_blank" rel="noopener noreferrer">
+            <Image src="/linkedin-icon.svg" alt="LinkedIn" width={24} height={24} />
+          </a>
+          <a href="mailto:natevella@example.com" className="ml-4">
+            <Image src="/email-icon.png" alt="Email" width={24} height={24} />
+          </a>
+          <a href="https://github.com/nathanvella" target="_blank" rel="noopener noreferrer" className="ml-4">
+            <Image src="/github-icon.svg" alt="GitHub" width={24} height={24} />
+          </a>
+        </div>
       </div>
     </main>
   );

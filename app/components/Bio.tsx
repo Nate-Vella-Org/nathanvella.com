fa3aff240b39b1664ea0266cfb222e0ce18b9ec8
n-a-t-e-d-a-w-g <nathangvella@gmail.com>
2024-05-06 19:17:55 -0600

    no bio

--- app/components/Bio.tsx
+++ app/components/Bio.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export function Bio() {
     return (
-        <p className="text-lg" style={{ maxWidth: '250px' }}>Just trying to build stuff </p>
+        <p className="text-lg" style={{ maxWidth: '250px' }}></p>
     );
 }
